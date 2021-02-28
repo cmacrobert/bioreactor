c159821b34aea5530447790c770069ce781c5196
Nat <fruitynut19@gmail.com>
2021-02-28 18:43:24 +0000

    I learned how to Object
    Yaaasss

--- asdfghjkl.py
+++ asdfghjkl.py
@@ -4,5 +4,19 @@ Created on Sun Feb 28 13:42:07 2021
 
 @author: User
 """
+class MrAgnewsClass():
+    spines = 9
+    
+    def __init__ (self, aardvark):
+        self.aardvark = aardvark
+        
+    def soap(self):
+        print("i am " + self.aardvark + ", i have " + str(self.spines) + " spines." )
+        
+      
+arthur = MrAgnewsClass("arthur")
+sue  = MrAgnewsClass("arthur2")
 
-#frnvfjfofjosdfdfdkgoidd
+if input() == "hello":
+    arthur.soap()
+    
