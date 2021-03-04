e6f60c82f89dbb506ef1dfec6bf477e89622d95b
Calum <cmacrobert@dundee.ac.uk>
2021-03-04 10:11:34 +0000

    Changed naming style to match standard

--- sensors/temperature.py
+++ sensors/temperature.py
@@ -5,10 +5,11 @@ Created on Sat Feb 20 13:15:50 2021
 @author: Calum
 """
 
-from sensors.base import sensorBase
+from sensors.base import SensorBase
 
-class sensorTemperature(sensorBase):
+class SensorTemperature(SensorBase):
     
     #Functions
     def __init__(self, label):
+        SensorBase.__init__(self);
         self.label = label
