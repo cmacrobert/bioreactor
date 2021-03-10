67940f5a8621eb21c4ed193295f8597000aa59eb
Nat <fruitynut19@gmail.com>
2021-03-10 19:24:27 +0000

    Thermocouple, MainProgram and Reactor
    Set up and you can talk to them.

---
+++ sensors/thermocouple.py
@@ -0,0 +1,20 @@
+from sensors.base import SensorBase
+class thermocouple(SensorBase):
+
+    def __init__(self, reactor):
+        super().__init__()
+        self.label = 'thermocouple'
+#needs to expect to be attached to a reactor    
+        self.reactor = reactor 
+        
+    def get_temperature(self):
+        self.set_sensor_value(self.reactor.gettemperature())
+        return super().get_sensor_value()
+        
+#test code for using fucntions defined in the parent class
+if __name__ == '__main__':
+    thermocouple = thermocouple()
+    print(thermocouple.get_sensor_value())
+    thermocouple.set_sensor_value(1000)
+    print(thermocouple.get_sensor_value())
+    
