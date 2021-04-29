dfe31f5b2afd758e7d9ce884f23b8f39f9bac827
Mirrin Gillespie <megillespie@dundee.ac.uk>
2021-04-29 18:29:37 +0100

    pressure, co2, reactor

--- reactor.py
+++ reactor.py
@@ -10,7 +10,7 @@ import time
 
 class reactor():
      
-    def __init__ (self, temperature=15, ph=7): 
+    def __init__ (self, temperature=15, ph=7, pressure=1, co2=5): 
         #self.name = name #argument name is passed thorugh init statement
         self.temperature = temperature 
         self.initialtemperature = temperature 
@@ -25,6 +25,16 @@ class reactor():
         self.name = "Reactor"
         self.P = ph
         self.T = temperature
+        
+        self.pressure = pressure
+        self.set_pressureinput = pressure
+        self.initialpressure = pressure
+        self.B = pressure
+        
+        self.co2 = co2
+        self.set_co2input = co2
+        self.initialco2 = co2
+        self.C = co2
         
     def get_running(self):
         return self.running
@@ -49,13 +59,19 @@ class reactor():
 #___________________________________________________________________
     #MIRRIN
     
-    #def get_pressure(pressure):
-     #   pass
+    def get_pressure(self):
+        print('Reactor - Getting pressure')
+        return self.pressure
  
     def set_pressure_input(self, pressure):
         self.set_pressureinput = pressure
-        #not yet used by reactor
-        pass
+#___________________________________________________________________
+def get_co2(self):
+        print('Reactor - Getting co2')
+        return self.co2
+ 
+    def set_co2_input(self, co2):
+        self.set_co2input = co2
 #___________________________________________________________________    
     def reset_temperature(self): 
         print('Reactor - Resetting temperature') 
@@ -149,6 +165,52 @@ class reactor():
         self.P = P
         self.ph = P[5]   
         
+        ###########################################
+        def reset_pressure(self): 
+        print('Reactor - Resetting pressure') 
+        self.pressure = self.initialpressure     
+    
+    def reactor_pressure_cycle(self):
+        B = self.B
+        
+        L = 0.21  # radius of sphere in m
+        n = 10  # number of divisions
+        B0 = 1  # assumes start reactor pressure is 1 
+        dx = L / n
+        alpha = 0.00229
+        t_final = 1  # time interval lasts one second
+        dt = 0.1  # this is the time step
+        x = np.linspace(dx / 2, L - dx / 2, n)
+        B = np.ones(n) * B0  # start pressure vector
+        # each to be taken from the last iteration
+        dBdt = np.empty(n)  # define empty vector
+        
+        t = np.arange(0, t_final, dt)
+        
+        for j in range(1, len(t)):  # looping for all elements in t
+            for i in range(1, n - 1):  # defines value of derivative for each spatial node
+                dBdt[i] = alpha * (
+                    -(B[i] - B[i - 1]) / dx ** 2 + (B[i + 1] - B[i]) / dx ** 2
+                )  # end nodes have boundary condition, left side
+            dBdt[0] = alpha * (
+                -(B[0] - self.pressure_into_reactor) / dx ** 2 + (B[0 + 1] - B[0]) / dx ** 2
+            )  # generic for inner nodes
+            dBdt[n-1] = alpha*(-(B[n-1]-B[n-1-1])/dx**2+(self.pressure_into_reactor*0.8-B[n-1])/dx**2) #the n-1 node
+           
+            B = B + dBdt * dt  # continuously update pressure vector, gets overwritten each time
+            # plt.figure(1) 
+            # plt.plot(x, B)
+            # plt.axis([0, L, 0, 10])
+            # plt.xlabel("Distance (m)")
+            # plt.ylabel("pressure")
+            # plt.show()
+            # plt.pause(0.05)
+                
+        # need to preserve Bs between cycles
+        self.B = B
+        self.pressure = B[5]   
+        #################################################
+        
     def start(self):
         self.running = True
        
@@ -156,6 +218,7 @@ class reactor():
             time.sleep(1)
             self.reactor_heating_cycle()                        
             self.reactor_ph_cycle()
+            self.reactor_pressure_cycle()
            
     def stop(self):
         print(self.name + ": Stopping thread")
